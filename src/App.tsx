53f6eed3a28809e02fbefdb3d52ad74ee5032643
krzykaczu <krzywiec@gmail.com>
2019-07-06 13:21:55 +0200

    Add Heroku's endpoint config

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import { ApolloProvider } from "react-apollo";
 import './App.css';
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000"
+    // uri: "http://localhost:4000"
+    uri: "https://company-turnover-server.herokuapp.com/"
 });
 
 interface MatchParams {
