30c24c935af23b6f25b9c2f71145a9995e1e1329
krzykaczu <krzywiec@gmail.com>
2019-07-06 12:59:25 +0200

    Add Heroku's config

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,16 @@ var _resolvers2 = _interopRequireDefault(_resolvers);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var options = {
+    port: process.env.PORT || 4000
+};
+
 var server = new _graphqlYoga.GraphQLServer({
     typeDefs: "./src/schema.graphql",
     resolvers: _resolvers2.default
 });
 
-server.start(function () {
-    return console.log("Server is running on http://localhost:4000");
+server.start(options, function (_ref) {
+    var port = _ref.port;
+    return console.log("Server started, listening on port " + port + " for incoming requests.");
 });
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { GraphQLServer } from "graphql-yoga"
 import resolvers from './src/resolvers'
 
+const options = {
+    port: process.env.PORT || 4000,
+}
+
 const server = new GraphQLServer({
     typeDefs: "./src/schema.graphql",
     resolvers
 })
 
-server.start(() => console.log(`Server is running on http://localhost:4000`));
+server.start(options, ({ port }) => console.log(`Server started, listening on port ${port} for incoming requests.`));
