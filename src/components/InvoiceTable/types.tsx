6b45303c2fc205f8946c1433ec294c89794d7509
krzykaczu <krzywiec@gmail.com>
2019-07-12 06:18:47 +0200

    Update type files structure

--- src/components/InvoiceTable/types.tsx
+++ src/components/InvoiceTable/types.tsx
@@ -1,5 +1,5 @@
 import {WithStyles} from "@material-ui/core";
-import { styles } from "../styles"
+import { styles } from "./styles"
 
 export interface Props extends WithStyles<typeof styles> {
     data: Array<{ id: string, issueDate: string, net: number}>
