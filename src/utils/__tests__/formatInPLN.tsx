c7031874cd3b3cac1f812f346fcf19d0d5213d14
krzykaczu <krzywiec@gmail.com>
2019-07-22 11:10:06 +0200

    Fix formatInPLN test

--- src/utils/__tests__/formatInPLN.tsx
+++ src/utils/__tests__/formatInPLN.tsx
@@ -1,4 +1,4 @@
-import { formatInPLN } from '../utils/formatInPLN'
+import { formatInPLN } from '../formatInPLN'
 
 test('Test formatInPLN 1000', () => {
     expect(formatInPLN(1000)).toBe("1,000 PLN");
