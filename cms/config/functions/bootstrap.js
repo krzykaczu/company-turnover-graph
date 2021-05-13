be8921842ad8442ff7969ace48b8c1c365d656e2
Kacper Krzywiec <krzywiec@gmail.com>
2021-05-13 11:01:12 +0200

    feat: add cms data population from csv-parser service endpoint

--- cms/config/functions/bootstrap.js
+++ cms/config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +10,49 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+const axios = require("axios");
+
+const one_by_one = (arr, syncFn) =>
+  arr.reduce(
+    (prom, arrItem) => prom.then(() => syncFn(arrItem)),
+    Promise.resolve()
+  );
+
+module.exports = async () => {
+  //delete all saved invoices
+  const invoices = await strapi.query("invoice").find();
+  if (invoices && Array.isArray(invoices)) {
+    one_by_one(invoices, ({ _id }) => {
+      strapi.query("invoice").delete({ _id });
+    });
+  }
+
+  // populate db with data from csv-parser service
+  const { data } = await axios.get("http://0.0.0.0:5000");
+  if (data && Array.isArray(data)) {
+    one_by_one(
+      data,
+      ({
+        id: invoiceId,
+        status,
+        issueDate,
+        warehouse,
+        client,
+        city,
+        net,
+        gross,
+      }) => {
+        strapi.query("invoice").create({
+          invoiceId,
+          status,
+          issueDate,
+          warehouse,
+          client,
+          city,
+          net,
+          gross,
+        });
+      }
+    );
+  }
+};
